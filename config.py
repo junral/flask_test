484ce437fcf0f65bd381ba04b6a3216c20a3b4c5
junral <junral@163.com>
2017-12-04 10:49:35 +0800

    add db

--- config.py
+++ config.py
@@ -1,14 +1,34 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import os
+
 
 class Config(object):
     pass
 
 
 class ProdConfig(object):
+    """ 生产环境配置 """
+    # MYSQL
+    # mysql+pymysql://user:password@ip:port/db_name
+    # Postgres
+    # postgresql+psycopg2://user:password@ip:port/db_name
+    # MSSQL
+    # mssql+pyodbc://user:password@dsn_name
+    # Oracle
+    # oracle+cx_oracle://user:password@ip:port/db_name
     pass
 
 
 class DevConfig(object):
+    """ 开发环境配置 """
     DEBUG = True
+    # SQLite
+    # BASE_URL = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # DB_URI = 'sqlite:///' + os.path.join(BASE_URL, 'database.db')
+    DB_URI = 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = DB_URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # 查看数据库 SQL 查询语句设置
+    # SQLALCHEMY_ECHO = True
--- main.py
+++ main.py
@@ -1,17 +1,128 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Flask
-from config import DevConfig
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
+from config import DevConfig
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    """ 用户表 """
+
+    # 数据库表明
+    __tablename__ = 'user'
+
+    # 主键
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    # 在 SQLAlchem 中创建虚拟的列，和 Post 表中的外键建立联系
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return r"<User '{}'>".format(self.username)
+
+
+# 针对 Post 表和 Tag 表的多对多关系管理
+tags = db.Table(
+    'tags',
+    db.Column('post_id', db.Integer(), db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer(), db.ForeignKey('tag.id'))
+)
+
+
+class Post(db.Model):
+    """ 博客文章表 """
+    __tablename__ = 'post'
+
+    # 主键
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime())
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
+    # 外键
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    # secondary 参数会告知 SQLAlchemy 该关联别保存在 tags 表里
+    tags = db.relationship(
+        'Tag',
+        secondary=tags,
+        backref=db.backref('posts', lazy='dynamic')
+    )
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return r"<Post'{}'>".format(self.title)
+
+
+class Comment(db.Model):
+    """ 评论表 """
+    __tablename__ = 'comment'
+
+    # 主键
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime())
+    # 外键
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
+
+
+class Tag(db.Model):
+    """ 标签表 """
+    __tablename__ = 'tag'
+
+    # 主键
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
+
+
+def sidebar_data():
+    recent = Post.query.order_by(Post.publish_date.desc()).limit(5).all()
+    top_tags = db.session.query(
+        Tag, func.count(tags.c.post_id).label('total')
+    ).join(
+        tags
+    ).group_by(Tag).order_by('total DESC').limit(5).all()
+
+    return recent, top_tags
 
 
 @app.route('/')
-def home():
-    return '<h1>Hello World!</h1>'
+@app.route('/<int:page>')
+def home(page=1):
+    # return '<h1>Hello World!</h1>'
+    posts = Post.query.order_by(
+        Post.publish_date.desc()
+    ).pagination(page, 10)
+    recent, top_tags = sidebar_data()
+
+    return render_template(
+        'home.html',
+        posts=posts,
+        recent=recent,
+        top_tags=top_tags
+    )
 
 
 if __name__ == '__main__':
